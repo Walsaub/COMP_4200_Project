48c106166f6e5f9f9321ff84fc1473f98c42cf1f
Wissam Al Saub <alsaubw@uwindsor.ca>
2026-03-16 04:11:32 -0400

    Created the bottom navigation system and the app skeleton

--- app/src/main/java/com/example/carsocialmedia/MainActivity.java
+++ app/src/main/java/com/example/carsocialmedia/MainActivity.java
@@ -7,6 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.carsocialmedia.fragments.HomeFragment;
+import com.example.carsocialmedia.fragments.MarketplaceFragment;
+import com.example.carsocialmedia.fragments.NewPostFragment;
+import com.example.carsocialmedia.fragments.ProfileFragment;
+import com.example.carsocialmedia.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +25,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        bottomNav.setOnItemSelectedListener( menuItem -> {
+            Fragment selectedFragment = null;
+            int id = menuItem.getItemId();
+
+            if (id == R.id.home_nav) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.marketplace_nav) {
+                selectedFragment = new MarketplaceFragment();
+            } else if (id == R.id.add_post_nav) {
+                selectedFragment = new NewPostFragment();
+            } else if (id == R.id.search_nav) {
+                selectedFragment = new SearchFragment();
+            } else if (id == R.id.profile_nav) {
+                selectedFragment = new ProfileFragment();
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
         });
     }
 }

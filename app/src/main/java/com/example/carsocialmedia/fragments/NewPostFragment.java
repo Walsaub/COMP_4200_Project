42cdbd0df00c03ba517fe2ea599b0af1a1f23eea
Omar Kadish <omarkadish@gmail.com>
2026-03-25 01:43:03 -0400

    Handle create post Activity functions

--- app/src/main/java/com/example/carsocialmedia/fragments/NewPostFragment.java
+++ app/src/main/java/com/example/carsocialmedia/fragments/NewPostFragment.java
@@ -1,13 +1,28 @@
 package com.example.carsocialmedia.fragments;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.carsocialmedia.EditProfileActivity;
 import com.example.carsocialmedia.R;
 
 /**
@@ -16,6 +31,13 @@ import com.example.carsocialmedia.R;
  * create an instance of this fragment.
  */
 public class NewPostFragment extends Fragment {
+
+    private CheckBox toggleSaleInfo;
+    private LinearLayout saleInfoSection;
+    private TextView uploadImageBtn;
+    private ImageView previewImage;
+
+    private Uri selectedImageUri;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +83,42 @@ public class NewPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_post, container, false);
+
+        uploadImageBtn = view.findViewById(R.id.uploadBtn);
+        previewImage = view.findViewById(R.id.selectedImg);
+        uploadImageBtn.setOnClickListener(v -> {
+            pickMedia.launch(new PickVisualMediaRequest.Builder()
+                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                    .build());
+        });
+
+        toggleSaleInfo = view.findViewById(R.id.chkbxSaleInfo);
+        saleInfoSection = view.findViewById(R.id.saleInfoSection);
+
+        toggleSaleInfo.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                saleInfoSection.setVisibility(View.VISIBLE);
+            } else {
+                saleInfoSection.setVisibility(View.GONE);
+            }
+        });
+        if(toggleSaleInfo.isChecked()) {
+            // save values to database
+        }
+        else{
+            // ignore other values
+        }
+        return view;
     }
+
+    private final ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                if (uri != null) {
+                    selectedImageUri = uri;
+                    previewImage.setImageURI(uri);
+                } else {
+                    Toast.makeText(NewPostFragment.this.getContext(),"No photo Selected", LENGTH_SHORT).show();
+                }
+            });
 }
